315e5fb79b3ab4f2eab1f309395456b024adc3a2
unknown <ermolov1337@gmail.com>
2021-12-13 17:54:58 +0300

    add change card

--- src/component/Main/Secondsreen/Filtr/Filtr.jsx
+++ src/component/Main/Secondsreen/Filtr/Filtr.jsx
@@ -3,17 +3,22 @@ import s from "../Filtr/Filtr.module.scss"
 
 function Filtr(props) {
 
+  let gg;
+
+  props.active ? gg = "#4EB4E6" : gg = "rgba(78, 180, 230, 0.25);"
+  console.log(gg)
+  
   return (
 
-    <div className={s.filtr}>
-      <div className={s.img}>
-        <img src={props.src} />
-      </div>
-      <div className={s.text}>
-        <p className={s.title}>{props.text}</p>
-      </div>
+    <div className={s.filtr} onClick={props.onClick} style={{ backgroundColor: {gg}}}>
+  < div className = { s.img } >
+    <img src={props.src} />
+      </div >
+  <div className={s.text}>
+    <p className={s.title}>{props.text}</p>
+  </div>
 
-    </div>
+    </div >
 
   )
 }
--- src/component/Main/Secondsreen/Secondsreen.jsx
+++ src/component/Main/Secondsreen/Secondsreen.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import s from "../Secondsreen/Secondsreen.module.scss"
 import Card from "./Card/Card";
 import Filtr from "./Filtr/Filtr";
 
 function Secondscreen() {
+
+  const [number, setNumber] = useState(0)
+
 
   return (
 
@@ -12,21 +16,37 @@ function Secondscreen() {
 
 
         <div className={s.filtrList}>
-          <Filtr src='../img/Filtr/biaz.svg' text="Бязь" />
-          <Filtr src='../img/Filtr/ranfors.svg' text="Ранформ" />
-          <Filtr src='../img/Filtr/satin.svg' text="Сатин" />
-          <Filtr src='../img/Filtr/stripe.svg' text="Страйп Сатин" />
-          <Filtr src='../img/Filtr/other.svg' text="Прочий текстиль" />
+          <Filtr src='../img/Filtr/biaz.svg' text="Бязь" onClick={() => { setNumber(0) }}
+            active = {number === 0 ? "active" : ""} />
+          <Filtr src='../img/Filtr/ranfors.svg' text="Ранформ" onClick={() => { setNumber(1) }}
+            active={number === 1 ? "active" : ""} />
+          <Filtr src='../img/Filtr/satin.svg' text="Сатин" onClick={() => { setNumber(2) }}
+            active={number === 2 ? "active" : ""} />
+          <Filtr src='../img/Filtr/stripe.svg' text="Страйп Сатин" onClick={() => { setNumber(3) }}
+            active={number === 3 ? "active" : ""} />
+          <Filtr src='../img/Filtr/other.svg' text="Прочий текстиль" onClick={() => { setNumber(4) }}
+            active={number === 4 ? "active" : ""} />
         </div>
 
+        
         <div className={s.cardBox}>
-          <Card background="../img/Card/image.png" state={2} discount={51}/>
-          <Card background="../img/Card/image.png" state={3} discount={51}/>
-          <Card background="../img/Card/image.png" state={4} discount={51}/>
-        </div>
-        
 
-        
+          {number === 0 ? <Card background="../img/Card/image.png" state={2} discount={51} /> : null}
+          {number === 1 ? <Card background="../img/Card/image.png" state={22} discount={61} /> : null}
+          {number === 2 ? <Card background="../img/Card/image.png" state={114} discount={41} /> : null}
+          {number === 3 ? <Card background="../img/Card/image.png" state={45} discount={32} /> : null}
+          {number === 4 ? <Card background="../img/Card/image.png" state={334} discount={77} /> : null}
+
+
+{/*           
+          <Card background="../img/Card/image.png" state={2} discount={51} />
+          <Card background="../img/Card/image.png" state={3} discount={51} />
+          <Card background="../img/Card/image.png" state={4} discount={51} /> */}
+          
+        </div>
+
+
+
       </div>
     </div>
 
